a38cd3c4ee07197fd08ccd06e0e29a2f71c96b7a
JFors-LUT <jarno.forsblom@student.lut.fi>
2025-07-01 18:46:16 +0300

    constant_level_change

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const LEVEL1_SCENE = preload("res://Scenes/level1.tscn")
+const LEVEL2_SCENE = preload("res://Scenes/level2.tscn")
 
 @onready var player = $Player
 @onready var current_level: Node = null
@@ -32,7 +33,20 @@ func _process(delta: float) -> void:
 	pass
 	
 func _on_all_coins_collected(next_level_path: String) -> void:
-	change_level(next_level_path)
+	if current_level and current_level.is_inside_tree():
+		current_level.queue_free()
+	var new_level = LEVEL2_SCENE.instantiate()
+	add_child(new_level)
+	current_level = new_level
+	
+	if current_level.has_signal("player_fell"):
+		current_level.connect("player_fell", Callable(self, "_on_player_fell"))
+	if current_level.has_signal("all_coins_collected"):
+		current_level.connect("all_coins_collected", Callable(self, "_on_all_coins_collected"))
+	if current_level.has_signal("player_dead"):
+		current_level.connect("player_dead", Callable(self, "_on_player_dead"))
+	
+	$Player.global_position = Vector2(0,0)
 
 func change_level(path: String) -> void:
 	if current_level and current_level.is_inside_tree():
