872b8f5f064fc943627a8e124aa6c75f62028b6d
JFors-LUT <jarno.forsblom@student.lut.fi>
2025-07-01 17:53:29 +0300

    player direction

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,22 +4,13 @@ class_name Player extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-const Y_LIMIT = 1000.0  # Arvo, jonka alle ei saa pudota
-const RESET_POSITION = Vector2(0, 0)  # Minne hahmo palautetaan
+var facing_direction_right := true
 
 func reset_to_start():
 	global_position = Vector2(0, 0)
 	velocity = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	
-	if Input.is_action_just_pressed("ui_left"):
-		$AnimatedSprite2D.scale.x = abs(scale.x)
-
-		
-	if Input.is_action_just_pressed("ui_right"):
-		$AnimatedSprite2D.scale.x = abs(scale.x) * -1
-		
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -32,16 +23,21 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction > 0:
 		$AnimatedSprite2D.play("run")
-			
-		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = true
+		facing_direction_right = true
+
+	elif direction < 0:
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = false 
+		facing_direction_right = false
 		
 	else:
 		$AnimatedSprite2D.play("idle")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.flip_h = facing_direction_right
 		
-	if $AnimatedSprite2D.global_position.y < -1000:
-		$AnimatedSprite2D.global_position = 0
+		
+	velocity.x = direction * SPEED
 		
 	move_and_slide()
